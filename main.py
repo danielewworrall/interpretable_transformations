4d253bc9c73631d1714ee8b427be887fe5d9a1db
Daniel <deworrall92@gmail.com>
2018-06-07 17:39:42 +0200

    well it trains

--- main.py
+++ main.py
@@ -14,24 +14,33 @@ from matplotlib import pyplot as plt
 from torchvision import datasets, transforms
 
 class Net(nn.Module):
-    def __init__(self, height, width):
+    def __init__(self, height, width, device):
         super(Net, self).__init__()
 
         self.height = height
         self.width = width
-        self.num_mid = int(height*width / 2)
+        self.device = device
 
-        self.down1 = nn.Linear(self.height*self.width, self.num_mid)
-        self.down2 = nn.Linear(self.num_mid, 10)
-        self.up2 = nn.Linear(10, self.num_mid)
-        self.up1 = nn.Linear(self.num_mid, self.height*self.width)
+        self.down1 = nn.Linear(self.height*self.width, 600)
+        self.down2 = nn.Linear(600, 400)
+        self.down3 = nn.Linear(400, 200)
+        self.up3 = nn.Linear(200, 400)
+        self.up2 = nn.Linear(400, 600)
+        self.up1 = nn.Linear(600, self.height*self.width)
 
 
-    def forward(self, x):
+    def forward(self, x, params):
         x = F.relu(self.down1(x))
-        x = self.down2(x)
+        x = F.relu(self.down2(x))
+        x = self.down3(x)
+
+        # Feature transform layer
+        x = self.feature_transformer(x, params)
+
+        x = F.relu(self.up3(x))
         x = F.relu(self.up2(x))
-        return self.up1(x)
+        return F.sigmoid(self.up1(x))
+
 
     def feature_transformer(self, input, params):
         """For now we assume the params are just a single rotation angle
@@ -47,9 +56,10 @@ class Net(nn.Module):
         # Construct the transformation matrix
         sin = torch.sin(params)
         cos = torch.cos(params)
-        matrix = torch.concat([sin, -cos, cos, sin], )
+        transform = torch.cat([sin, -cos, cos, sin], 1)
+        transform = transform.view(transform.size(0),1,2,2).to(self.device)
         # Multiply
-        output = transformation @ x
+        output = torch.matmul(transform, x)
         # Reshape and return
         return output.view(input.size())
 
@@ -59,14 +69,17 @@ def train(args, model, device, train_loader, optimizer, epoch):
     for batch_idx, (data, target) in enumerate(train_loader):
         # Reshape data
         data = data.view(data.size(0), -1)
+        params = torch.zeros(args.batch_size, 1)
 
         # Forward pass
         data = data.to(device)
         optimizer.zero_grad()
-        output = model(data)
+        output = model(data, params)
 
         # L1 loss
-        loss = torch.mean(torch.sum(torch.abs(output - data), 1))
+        loss_fnc = nn.BCELoss(size_average=False)
+        loss = loss_fnc(output, data)
+        #loss = torch.mean(torch.sum(torch.abs(output - data), 1))
 
         # Backprop
         loss.backward()
@@ -78,28 +91,28 @@ def train(args, model, device, train_loader, optimizer, epoch):
             sys.stdout.flush()
 
 
-def test(args, model, device, test_loader):
+def test(args, model, device, test_loader, epoch):
     model.eval()
     with torch.no_grad():
         for data, target in test_loader:
             # Reshape data
             data = data.view(data.size(0), -1)
+            params = torch.zeros(args.test_batch_size, 1)
 
             # Forward pass
             data = data.to(device)
-            output = model(data)
+            output = model(data, params)
             break
-        print(output.size())
+        output = output.cpu()
+        output = output.view(-1,1,28,28)
+        save_images(output, epoch)
 
 
-
-
-
-def view_images(images, nrow=None):
-    """View the images in a grid format
+def save_images(images, epoch, nrow=None):
+    """Save the images in a grid format
 
     Args:
-        images: array of shape [N,rgb,h,w], rgb=1 or 3
+        images: array of shape [N,1,h,w], rgb=1 or 3
     """
     if nrow == None:
         nrow = int(np.floor(np.sqrt(images.size(0))))
@@ -107,7 +120,10 @@ def view_images(images, nrow=None):
     img = torchvision.utils.make_grid(images, nrow=nrow, normalize=True).numpy()
     img = np.transpose(img, (1,2,0))
 
+    plt.figure()
     plt.imshow(img)
+    plt.savefig("./output/epoch{:04d}".format(epoch))
+    plt.close()
 
 def main():
     # Training settings
@@ -118,8 +134,8 @@ def main():
                         help='input batch size for testing (default: 100)')
     parser.add_argument('--epochs', type=int, default=100, metavar='N',
                         help='number of epochs to train (default: 100)')
-    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
-                        help='learning rate (default: 0.01)')
+    parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
+                        help='learning rate (default: 0.001)')
     parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
                         help='SGD momentum (default: 0.9)')
     parser.add_argument('--no-cuda', action='store_true', default=False,
@@ -139,24 +155,22 @@ def main():
     train_loader = torch.utils.data.DataLoader(
         datasets.MNIST('../data', train=True, download=True,
                        transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
+                           transforms.ToTensor()
                        ])),
         batch_size=args.batch_size, shuffle=True, **kwargs)
     test_loader = torch.utils.data.DataLoader(
         datasets.MNIST('../data', train=False, transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
+                           transforms.ToTensor()
                        ])),
         batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
-    model = Net(28,28).to(device)
-    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    model = Net(28,28,device).to(device)
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
     for epoch in range(1, args.epochs + 1):
 
         train(args, model, device, train_loader, optimizer, epoch)
-        test(args, model, device, test_loader)
+        test(args, model, device, test_loader, epoch)
 
 
 if __name__ == '__main__':
